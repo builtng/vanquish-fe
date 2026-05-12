3ed31bd75096b9130ac42420e0453217f5b03aa9
Charlesaloaye <admin@gen6ixx.com>
2026-05-12 11:54:53 +0100

    chore: update database config and env example

--- backend/config/database.php
+++ backend/config/database.php
@@ -61,6 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'isolation_level' => env('DB_ISOLATION_LEVEL', 'REPEATABLE READ'),
         ],
 
         'mariadb' => [
